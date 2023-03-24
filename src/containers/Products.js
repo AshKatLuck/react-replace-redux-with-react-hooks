7f45253ec6067eac4583e1100a43369ac194ce3f
Asha Luckins <ashaluckinsanoop@gmail.com>
2023-03-23 21:00:55 -0400

    add context to Products

--- src/containers/Products.js
+++ src/containers/Products.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React from "react";
 
-import ProductItem from '../components/Products/ProductItem';
-import './Products.css';
+import ProductItem from "../components/Products/ProductItem";
+import "./Products.css";
+import { ProductsContext } from "../context/products-context";
+import { useContext } from "react";
 
-const Products = props => {
-  const productList = useSelector(state => state.shop.products);
+const Products = (props) => {
+  const productList = useContext(ProductsContext).products;
+  console.log(productList);
   return (
     <ul className="products-list">
-      {productList.map(prod => (
+      {productList.map((prod) => (
         <ProductItem
           key={prod.id}
           id={prod.id}
