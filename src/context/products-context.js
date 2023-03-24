815b683359f4628e6dfbbf76f86d6bb498d0a80c
Asha Luckins <ashaluckinsanoop@gmail.com>
2023-03-23 21:18:59 -0400

    fix the Favorite button and page using context

--- src/context/products-context.js
+++ src/context/products-context.js
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
-export const ProductsContext = React.createContext({ products: [] });
+export const ProductsContext = React.createContext({
+  products: [],
+  toggleFav(id) {},
+});
 
 export default (props) => {
   const [productList, setProductList] = useState([
@@ -28,9 +31,19 @@ export default (props) => {
       isFavorite: false,
     },
   ]);
+  const ToggleFavorite = (productId) => {
+    setProductList((currentProdList) => {
+      const prodIndex = currentProdList.findIndex((p) => p.id === productId);
+      const newFavStatus = !currentProdList[prodIndex].isFavorite;
+      const updatedProducts = [...currentProdList];
+      updatedProducts[prodIndex].isFavorite = newFavStatus;
+      return updatedProducts;
+    });
+  };
   return (
-    <ProductsContext.Provider value={{ products: productList }}>
-      {/* {console.log({ productList })} */}
+    <ProductsContext.Provider
+      value={{ products: productList, toggleFav: ToggleFavorite }}
+    >
       {props.children}
     </ProductsContext.Provider>
   );
